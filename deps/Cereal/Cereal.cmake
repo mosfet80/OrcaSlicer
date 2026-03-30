6071c3454abd787f2d665ee6773bf60bfff5bce2
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2026-03-30 16:32:20 +0200

    Update Cereal version to 1.3.2
    Highlighted fixes and enhancements include:
    Github actions in place of Travis CI (thanks to  isuruf,  727)
    Doctest updates (and upstream patches!) to support all targeted compilers ( 714,  736)
    Cmake modernization (thanks to  ClausKlein,  659)
    Bug fixes and minor enhancements:
    Fix typo in docs by  tankorsmash in  597
    Add MSVC 2019 to build, default ctor for static object by  AzothAmmo in  593
    Fix json.hpp compilation issue when int32_t is a long by  bblackham in  621
    [cpp20] explicitly capture 'this' as copy by  lukaszgemborowski in  640
    Fix rapidjson for Clang 10 by  groscoe2 in  645
    Fixes to prevent clang-diagnostic errors by  johngladp in  643
    cleanup cmake files to be a little more moderen by  ClausKlein in  659
    CVE-2020-11105: Store a copy of each serialized shared_ptr within the archive to prevent the shared_ptr to be freed to early. by  serpedon in  667
    add license files for components of cereal by  miartad in  676
    Catch short documents in JSON input by  johnkeeping in  677
    C++17: use inline globals for StaticObjects by  InBetweenNames in  657
    Use std::variant::emplace when loading by  kepler-5 in  699
    Use std::optional::emplace() when loading non-empty optional by  kepler-5 in  698
    Fix itsNextName not clearing when not found + style change by  AzothAmmo in  715
    Update doctest to 2.4.6 + local fixes slated for upstream by  AzothAmmo in  716
    Fixed loading of std::vector by  Darred in  732
    Update license to match BSD template by  AzothAmmo in  735
    Update doctest to 2.4.7 by  AzothAmmo in  736
    Use GNUInstallDirs instead of hard wiring install directories by  antonblanchard in  710

--- deps/Cereal/Cereal.cmake
+++ deps/Cereal/Cereal.cmake
@@ -3,8 +3,8 @@
 #/|/ PrusaSlicer is released under the terms of the AGPLv3 or higher
 #/|/
 orcaslicer_add_cmake_project(Cereal
-    URL "https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.0.zip"
-    URL_HASH SHA256=71642cb54658e98c8f07a0f0d08bf9766f1c3771496936f6014169d3726d9657
+    URL "https://github.com/USCiLab/cereal/archive/refs/tags/v1.3.2.zip"
+    URL_HASH SHA256=e72c3fa8fe3d531247773e346e6824a4744cc6472a25cf9b30599cd52146e2ae
     CMAKE_ARGS
         -DJUST_INSTALL_CEREAL=ON
         -DSKIP_PERFORMANCE_COMPARISON=ON
